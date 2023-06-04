a6b944878439e69f174c0dffaf94b36bf6794234
TheCaptain333 <106683739+TheCaptain333@users.noreply.github.com>
2023-06-05 00:23:00 +0300

    initial commit

---
+++ player.py
@@ -0,0 +1,41 @@
+from utils import Player,WINDOW_WIDTH
+import cv2
+import numpy as np
+
+
+player = Player()
+    #Initializing a Player object with random location on a randomly generated Maze
+
+def strategy():
+    #This function is to localize the position of the newly created player with respect to the map
+    pass
+
+
+if __name__ == "__main__":
+    strategy()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
